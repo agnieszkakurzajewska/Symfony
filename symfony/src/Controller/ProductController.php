59333c8faf9f2148319df517ed4b987a69be7e22
Agnieszka Kurzajewska <agnieszka.kurzajewska@cloudservices.pl>
2021-07-16 11:50:32 +0200

    added product get

--- symfony/src/Controller/ProductController.php
+++ symfony/src/Controller/ProductController.php
@@ -7,13 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProductController extends AbstractController
 {
     /**
      * @Route("/product", name="create_product")
      */
-    public function createProduct(): Response
+    public function createProduct(ValidatorInterface $validator): Response
     {
         // you can fetch the EntityManager via $this->getDoctrine()
         // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
@@ -29,7 +30,34 @@ class ProductController extends AbstractController
         // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
 
+        $errors = $validator->validate($product);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
+
         return new Response('Saved new product with id '.$product->getId());
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function show(int $id): Response
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a templatecom
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
     }
 
 }
--- useful_files/README.txt
+++ useful_files/README.txt
@@ -1,2 +1,4 @@
 http://localhost:80
 http://localhost:5555 - pgAdmin {pgadmin4@pgadmin.org} {root}
+ php bin/console doctrine:query:sql 'SELECT * FROM product'
+
